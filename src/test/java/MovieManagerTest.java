53ca6989dcfbc61b66f406c4b83cedd018b90549
Mikhail <mpolet@outlook.com>
2023-03-07 18:40:49 +0300

    Add new test

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -35,7 +35,7 @@ public class MovieManagerTest {
     }
 
     @Test
-    public void addLastMovieIfMoviesQtyThatEqualsLimit(){
+    public void addLastMovieIfMoviesQtyThatEqualsLimit() {
         MovieManager manager = new MovieManager(10);
 
         manager.addMovie("Test 1");
@@ -51,13 +51,13 @@ public class MovieManagerTest {
 
 
         String[] actual = manager.findLast(10);
-        String[] expected = {"Test 10", "Test 9", "Test 8", "Test 7","Test 6", "Test 5", "Test 4", "Test 3", "Test 2", "Test 1"};
+        String[] expected = {"Test 10", "Test 9", "Test 8", "Test 7", "Test 6", "Test 5", "Test 4", "Test 3", "Test 2", "Test 1"};
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void addMoviesIfMoviesQtyThatEqualsLimit(){
+    public void addMoviesIfMoviesQtyThatEqualsLimit() {
         MovieManager manager = new MovieManager();
 
         manager.addMovie("Test 1");
@@ -73,7 +73,30 @@ public class MovieManagerTest {
 
 
         String[] actual = manager.findAll();
-        String[] expected = {"Test 1", "Test 2", "Test 3", "Test 4","Test 5", "Test 6", "Test 7", "Test 8", "Test 9", "Test 10"};
+        String[] expected = {"Test 1", "Test 2", "Test 3", "Test 4", "Test 5", "Test 6", "Test 7", "Test 8", "Test 9", "Test 10"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addLastMovieIfMoviesMoreThanLimit() {
+        MovieManager manager = new MovieManager(10);
+
+        manager.addMovie("Test 1");
+        manager.addMovie("Test 2");
+        manager.addMovie("Test 3");
+        manager.addMovie("Test 4");
+        manager.addMovie("Test 5");
+        manager.addMovie("Test 6");
+        manager.addMovie("Test 7");
+        manager.addMovie("Test 8");
+        manager.addMovie("Test 9");
+        manager.addMovie("Test 10");
+        manager.addMovie("Test 11");
+
+
+        String[] actual = manager.findLast(10);
+        String[] expected = {"Test 11", "Test 10", "Test 9", "Test 8", "Test 7", "Test 6", "Test 5", "Test 4", "Test 3", "Test 2"};
 
         Assertions.assertArrayEquals(expected, actual);
     }
